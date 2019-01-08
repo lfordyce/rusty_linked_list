29339dc9d22f920078d2e80787ad894a26dc127d
LFordyc1 <Lee.Fordyce1@T-Mobile.com>
2019-01-08 16:48:44 -0700

    Updated package structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate linked_list;
+
+use linked_list::third::List;
+
 fn main() {
-    println!("Hello, world!");
+    let list = List::new();
+    let list = list.append(1).append(2).append(3);
+    println!("{:?}", list.head().unwrap());
 }
